ecbc14035524fcdcf22dd094fcd95dfa37cb3d13
Huanderson C. Ferreira <huandersonferreira7@gmail.com>
2026-03-07 15:55:43 -0300

    enable and configure openssh

--- configuration.nix
+++ configuration.nix
@@ -89,7 +89,11 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
+  services.openssh.settings = {
+    PasswordAuthentication = true;
+    PermitRootLogin = "no";
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
