859527bc650065155ba72ded1fb994e02c3c03ac
Huanderson C. Ferreira <huandersonferreira7@gmail.com>
2026-03-07 15:16:24 -0300

    install and configure KDE

--- configuration.nix
+++ configuration.nix
@@ -35,8 +35,9 @@
   # };
 
   # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
+  services.xserver.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   
 
