7360909096f504839061fb2b3779919ec08c58d0
Huanderson C. Ferreira <huandersonferreira7@gmail.com>
2026-03-07 15:45:07 -0300

    enable unfree packages

--- configuration.nix
+++ configuration.nix
@@ -41,6 +41,8 @@
 
   hardware.graphics.enable = true;  
 
+  nixpkgs.config.allowUnfree = true;
+
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
   # services.xserver.xkb.options = "eurosign:e,caps:escape";
