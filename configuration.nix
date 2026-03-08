31868263a1555161d3f2a9e05013b8982510670d
Huanderson C. Ferreira <huandersonferreira7@gmail.com>
2026-03-08 09:19:26 -0300

    adiciona pacotes vscode e discord ao sistema

--- configuration.nix
+++ configuration.nix
@@ -79,6 +79,8 @@
    environment.systemPackages = with pkgs; [
      git
      google-chrome
+     vscode
+     discord
   #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #   wget
    ];
