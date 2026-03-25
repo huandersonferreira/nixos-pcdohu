fc0a3483fe9655c7cb3f53c8c9bdfacb1c6178c4
Huanderson C. Ferreira <huandersonferreira7@gmail.com>
2026-03-25 14:30:22 -0300

    adiciona variáveis de sessão para configuração do Chromium

--- profiles/desktop.nix
+++ profiles/desktop.nix
@@ -5,6 +5,10 @@
     ../modules/desktop/kde.nix
   ];
 
+  environment.sessionVariables = {
+    CHROMIUM_FLAGS = "--disable-gpu-sandbox --use-gl=swiftshader";
+  };
+
   environment.systemPackages = with pkgs; [
     discord
     google-chrome
