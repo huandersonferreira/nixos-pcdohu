d63b4249f40b0ae2539f29b385f9296ccea1d638
Huanderson C. Ferreira <huandersonferreira7@gmail.com>
2026-04-12 10:05:28 -0300

    adiciona configuração do Docker e pacotes ao ambiente

--- roles/docker-host.nix
+++ roles/docker-host.nix
@@ -1,7 +1,19 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   virtualisation.docker = {
     enable = true;
+    enableOnBoot = true;
+    daemon.settings = {
+      features = {
+        buildkit = true;
+      };
+    };
   };
+
+  environment.systemPackages = with pkgs; [
+    docker
+    docker-compose
+    docker-buildx
+  ];
 }
